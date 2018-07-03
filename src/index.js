158fcf6dc9a22e629ef7df1e629698b8b7b6d162
Edu <eduugr@gmail.com>
2018-07-03 12:25:58 -0300

    Allow passing options to JOI validate

--- src/index.js
+++ src/index.js
@@ -1,23 +1,28 @@
 const Joi = require('joi');
 const joia = Object.assign({}, Joi);
 
-const promisifier = (schema, obj) =>
+const promisifier = (schema, obj, options) =>
   new Promise((resolve, reject) => {
-    Joi.validate(obj, schema, (err, value) => {
-      if (err) return reject(Object.assign({error: true, ...err.details[0]}));
+    options = options || {};
+    Joi.validate(obj, schema, options, (err, value) => {
+      if (err) {
+        err = !options.abortEarly ? err.details : err.details[0];
+        return reject(Object.assign({error: true, ...err}));
+      }
       resolve(value);
     });
   });
 
-const validate = schema => values =>
+const validate = (schema, options) => values =>
   Array.isArray(values)
-    ? Promise.all(values.map(v => promisifier(schema, v).catch(e => e)))
-    : promisifier(schema, values);
+      ? Promise.all(values.map(v => promisifier(schema, v, options).catch(e => e)))
+      : promisifier(schema, values, options);
 
-joia.createSchema = schema => {
+
+joia.createSchema = (schema, options) => {
   return {
     schema,
-    validate: validate(schema)
+    validate: validate(schema, options)
   };
 };
 
